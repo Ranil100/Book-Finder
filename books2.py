ffd795c7adf4b534e0ba718e820998f2272e8b2e
Ranil100 <ranil885985792@gmail.com>
2026-05-29 10:11:50 +0530

    Added data Validation using Pydantic

--- books2.py
+++ books2.py
@@ -1,21 +1,24 @@
-from fastapi import FastAPI, Body
+from dataclasses import field
+from fastapi import FastAPI
+from pydantic import BaseModel,Field
 
 app = FastAPI()
 
 class Book():
-    id : int
-    title : str
-    author : str
-    description : str
-    rating : str
-
-    def __init__(self ,id ,title, author, description,rating):
+   def __init__(self ,id ,title, author, description,rating):
       self.id = id
       self.title = title
       self.author = author
       self.description = description
       self.rating = rating
 
+
+class BookRequest(BaseModel):
+        id: int
+        title: str = Field(min_length = 3)
+        author: str = Field(min_length = 3)
+        description: str = Field(min_length = 3,max_length= 100)
+        rating: int = Field(gt= 0, lt= 6)
 
 BOOKS = [
     Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
@@ -30,6 +33,18 @@ BOOKS = [
 async def read_all_books():
     return BOOKS
 
-@app.post("/create-book")
-async def create_book(book_request = Body()):
-     BOOKS.append(book_request)
+
+@app.post("/create")
+async def create_book(book_request: BookRequest):
+     new_book = Book(**book_request.dict())
+     find_book_id(new_book)
+     BOOKS.append(new_book)
+
+def find_book_id(book : Book):
+    if len(BOOKS) > 0:
+        Book.id = BOOKS[-1].id + 1
+    else:
+        Book.id = 1
+
+    return book
+
